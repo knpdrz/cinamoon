10fef96472ba3613e5d340cb2c9925449dc53362
Monika Ruszczyk <monika.ruszczyk@allegro.pl>
2021-01-24 11:33:50 +0100

    add logging

--- src/main/java/mrz/screening/ScreeningController.java
+++ src/main/java/mrz/screening/ScreeningController.java
@@ -1,5 +1,7 @@
 package mrz.screening;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @RestController
 public class ScreeningController {
@@ -17,12 +17,12 @@ public class ScreeningController {
     @Autowired
     ScreeningsService screeningsService;
 
-    private final static Logger LOG = Logger.getLogger(ScreeningController.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(ScreeningController.class.getName());
 
     @GetMapping("/screenings")
     public List<ScreeningsDto> getScreeningsBetween(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateFrom,
                                                     @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime dateTo) {
-        LOG.log(Level.INFO, "getting screeinngs between " + dateFrom + " and " + dateTo);
+        LOG.info("getting screeinngs between " + dateFrom + " and " + dateTo);
         return screeningsService.getScreeningsBetween(dateFrom, dateTo);
     }
 
